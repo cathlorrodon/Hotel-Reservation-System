7c96be85bdec1cf6ba43a567176000f3814f716c
Maria Yuki Villanueva <116412167+mariayukiv@users.noreply.github.com>
2023-06-14 18:10:02 +0800

    Add files via upload

---
+++ Room_Reservation/Main.java
@@ -0,0 +1,21 @@
+
+import javax.swing.SwingUtilities;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+     public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new RoomReservationSystem();
+            }
+        });
+    }
+}
