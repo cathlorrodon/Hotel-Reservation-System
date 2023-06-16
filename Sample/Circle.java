0fed608eba74d280b29c327f05146e8e7b1b66d9
Maria Yuki Villanueva <116412167+mariayukiv@users.noreply.github.com>
2023-06-16 22:32:26 +0800

    Add files via upload

---
+++ Sample/Circle.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Circle extends Polygon {
+    public void render () {
+    
+    System.out.println ("Rendering a Polygon"); 
+    
+}
+
+  
+}
