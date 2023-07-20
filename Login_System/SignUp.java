b0f2375fca1288f08f09051b59c318a679a1b40d
KennethOdgien <nethkenzodgien@gmail.com>
2023-07-20 19:13:04 +0800

    Update SignUp.java
    added some changes

--- Login_System/SignUp.java
+++ Login_System/SignUp.java
@@ -1,9 +1,10 @@
-package Login_System;
+package hotelLogin;
 
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
@@ -13,12 +14,16 @@ import javax.swing.JTextField;
  *
  * @author Kenneth Odgien
  */
-public class SignUp extends Accounts implements ActionListener{
+public class SignUp implements ActionListener{
     
     JFrame frame = new JFrame();
     JLabel HeadS = new JLabel("REGISTRATION FORM");
-    JTextField nameID = new JTextField();
+    JLabel firstnameLabel = new JLabel("First name: ");
+    JTextField firstnameID = new JTextField();
+    JLabel lastnameLabel = new JLabel("Last name: ");
+    JTextField lastnameID = new JTextField();
     JLabel nameLabel = new JLabel("Username: ");
+    JTextField nameID = new JTextField();
     JPasswordField password = new JPasswordField("");
     JLabel passwordLabel = new JLabel("Password: ");
     JPasswordField confirmpass = new JPasswordField();
@@ -27,12 +32,12 @@ public class SignUp extends Accounts implements ActionListener{
     JLabel emailLabel = new JLabel("Email: ");
     JButton submitBtn = new JButton("Submit");
     JLabel messageLabel = new JLabel();
+    JCheckBox terms = new JCheckBox("By using this app/site, you understand and agree to the terms");
     
-    //Constructor
     SignUp(){
         
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setSize(520, 420);
+	frame.setSize(520, 620);
 	frame.setLayout(null);
 	frame.setVisible(true);
         
@@ -40,24 +45,36 @@ public class SignUp extends Accounts implements ActionListener{
         HeadS.setBounds(100,20,1000, 40);
         
         //Labels
-        nameLabel.setBounds(60,100,80,25);
-        passwordLabel.setBounds(60,150,80,25);
-        confirmpassLabel.setBounds(12,200, 200,25);
-        emailLabel.setBounds(85,250,80,25);
-        messageLabel.setBounds(125,250,250,35);
+        firstnameLabel.setBounds(60,100,80,25);
+        lastnameLabel.setBounds(60,150,80,25);
+        nameLabel.setBounds(60,200,80,25);
+        passwordLabel.setBounds(60,250,80,25);
+        confirmpassLabel.setBounds(12,300, 200,25);
+        emailLabel.setBounds(85,350,80,25);
+        messageLabel.setBounds(125,350,250,35);
         messageLabel.setFont(new Font("Klavika",Font.ITALIC,15));
         
         //Fields
-        nameID.setBounds(140,100,200,25);
-        password.setBounds(140,150,200,25);
-        confirmpass.setBounds(140,200,200,25);
-        emailAdd.setBounds(140,250,200,25);
+        firstnameID.setBounds(140,100,200,25);
+        lastnameID.setBounds(140,150,200,25);
+        nameID.setBounds(140,200,200,25);
+        password.setBounds(140,250,200,25);
+        confirmpass.setBounds(140,300,200,25);
+        emailAdd.setBounds(140,350,200,25);
         
-        submitBtn.setBounds(130,300,100,25);
+        submitBtn.setBounds(130,420,100,25);
         submitBtn.setFocusable(false);
+        
+        terms.setFont(new Font("Arial", Font.PLAIN, 10));
+        terms.setBounds(135, 380, 500, 20);
+        terms.addActionListener(this);
         
                 
         frame.add(HeadS);
+        frame.add(firstnameLabel);
+        frame.add(lastnameLabel);
+        frame.add(firstnameID);
+        frame.add(lastnameID);
         frame.add(nameLabel);
         frame.add(passwordLabel);
         frame.add(confirmpassLabel);
@@ -67,7 +84,7 @@ public class SignUp extends Accounts implements ActionListener{
         frame.add(confirmpass);
         frame.add(emailAdd);
         frame.add(submitBtn);
-        
+        frame.add(terms);
         
     }
 
